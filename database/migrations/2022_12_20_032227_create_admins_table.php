107f1472b187353977138e6fe3819be9d22d2ff3
yellhtutaung <yellhtut4@gmail.com>
2022-12-20 09:56:29 +0630

    admins migration and model

---
+++ database/migrations/2022_12_20_032227_create_admins_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username')->nullable(); // autogenerate name id
+            $table->integer('role')->default(1);
+            $table->string('phone')->unique()->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('is_ban')->default(1); // zero is banded user
+            $table->integer('hide_show')->default(1); // zero is hidden user
+            $table->integer('status')->default(1);
+            $table->integer('upd_id')->nullable();
+            $table->integer('reg_id')->nullable();
+            $table->string('token')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
