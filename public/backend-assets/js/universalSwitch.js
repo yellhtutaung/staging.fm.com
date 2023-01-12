8916d6744dfabcc1ac57f04c874fda6494323fb7
yellhtutaung <yellhtut4@gmail.com>
2023-01-12 14:04:07 +0630

    switch field fix

--- public/backend-assets/js/universalSwitch.js
+++ public/backend-assets/js/universalSwitch.js
@@ -2,7 +2,7 @@ let universalSwitch = (Index,findId,field,on_off) =>
 {
     let orgVal = $('.toggle-id'+findId).attr("data-status-val");
     on_off = on_off==1?0:1;
-    console.log(on_off); // flip to data
+    // console.log(on_off); // flip to data
     let data_obj = {Index:Index,findId:findId,field:field,on_off:on_off}
     $.post(universalSwitchRoute,
     {
@@ -12,7 +12,7 @@ let universalSwitch = (Index,findId,field,on_off) =>
     function(data){
         if(data.status == 200)
         {
-            $('.toggle-id'+findId).attr("onchange",`universalSwitch(2,${findId},'hide_show',${on_off})`);
+            $('.toggle-id'+findId).attr("onchange",`universalSwitch(${Index},${findId},"${field}",${on_off})`);
             $('.toggle-id'+findId).attr("data-status-val",on_off);
         }
         console.log(data);
