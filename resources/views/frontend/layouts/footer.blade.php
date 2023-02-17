ff909a93d288e6667d79eceafbccf8d008d0363a
aungzawphyo <aungzawphyo1102@gmail.com>
2023-02-17 16:52:19 +0630

    fix toast alert

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -128,17 +128,19 @@
         $('#show-message').append(uiAlert);
     }
 
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'bottom-end',
-        showConfirmButton: false,
-        timer: 5000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-    });
+    // const Toast = Swal.mixin({
+    //     toast: true,
+    //     position: 'bottom-end',
+    //     showConfirmButton: false,
+    //     timer: 5000,
+    //     timerProgressBar: true,
+    //     didOpen: (toast) => {
+    //         toast.addEventListener('mouseenter', Swal.stopTimer)
+    //         toast.addEventListener('mouseleave', Swal.resumeTimer)
+    //     }
+    // });
+
+    // Toast is already been declared.
 
     $('#contact-form').on('submit', function(e){
         e.preventDefault();
