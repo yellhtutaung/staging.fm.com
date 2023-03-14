709b0d2f5eb6a19429e8082b7149c21d17c7d8f0
yellhtutaung <yellhtut4@gmail.com>
2023-03-14 17:02:43 +0630

    permission middleware

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Helpers\General;
+use App\Models\Permission;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
@@ -10,6 +11,8 @@ class TestController extends Controller
     use General;
     public function test()
     {
+        $get = Permission::orderBy('order_sort_id', 'desc')->first();
+        return $get->order_sort_id;
         return $this->findIdWithModelIndex(2,1,'hide_show',0);
     }
 }
