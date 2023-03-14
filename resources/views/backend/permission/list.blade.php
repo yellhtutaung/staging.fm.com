43cd920d61493ade49174bfe0bd356174e508707
aungzawphyo <aungzawphyo1102@gmail.com>
2023-03-14 17:00:58 +0630

    permission dragable

--- resources/views/backend/permission/list.blade.php
+++ resources/views/backend/permission/list.blade.php
@@ -1,6 +1,22 @@
 @extends('backend.layouts.app')
 @section('title', 'Permissions')
 @section('extra-css')
+    <style>
+        #sortable { list-style-type: none; margin: 0; padding: 0; width: 100%; }
+        #sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; height: 18px; }
+        #sortable li span { position: absolute; margin-left: -1.3em; }
+        tbody tr {
+            cursor: -webkit-grabbing;
+            cursor: grabbing;
+        }
+    </style>
+{{--    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>--}}
+{{--    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>--}}
+{{--    <script>--}}
+{{--        $( function() {--}}
+{{--            $( "#sortable" ).sortable();--}}
+{{--        } );--}}
+{{--    </script>--}}
 @endsection
 @section('content')
     <div class="container-fluid ">
@@ -11,12 +27,12 @@
             <h5 class="card-title text-white">Permissions</h5>
         </div>
         <div class="card card-custom table-responsive border-0 shadow-sm card-body animate__animated animate__fadeIn">
-            <table id="example" class="table table-responsive table-hover table-striped" >
+            <table id="example" class="table table-responsive table-hover" >
                 <thead>
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
-                    <th>Notes</th>
+                    <th hidden>Order</th>
                     <th>Permission Count</th>
                     <th>Reg Date</th>
                     <th>Updated</th>
@@ -26,14 +42,14 @@
                     <th>Delete</th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody  id="sortable">
                 @foreach($permissions as $Index => $permission)
-                    <tr>
+                    <tr class="ui-state-default order_sort_id" id="item-{{$permission->id}}" data-sort-order-id="{{$permission->order_sort_id??'-'}}">
                         <td>{{$Index+1}}</td>
-                        <td>{{$permission->name}}</td>
-                        <td>{{$permission->notes??'-'}}</td>
+                        <td class="id" value="{{$permission->id}}">{{$permission->name}}</td>
+                        <td hidden class="order_sort_id">{{$permission->order_sort_id}}</td>
                         <td>
-                            <span class="badge bg-warning">{{count(json_decode($permission->guard_json))}}</span>
+                            <span class="badge bg-danger">{{count(json_decode($permission->guard_json))}}</span>
                         </td>
                         <td>{{$permission->created_at->toDateString()}}</td>
                         <td>{{$permission->updated_at->diffForHumans()}}</td>
@@ -72,10 +88,13 @@
 
 @section('extra-script')
     <script src="{{asset('backend-assets/js/universalSwitch.js')}}" ></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
     <script>
+
         $(document).ready(function () {
             $('#example').DataTable({
                 "pageLength": 100,
+                "sort": false
             });
             $('.form-control-sm').addClass("input-data-search")
             $('.input-data-search').focus(); // input auto focus
@@ -86,5 +105,46 @@
         const delPermission = (id) => {
             $('#delPermissionForm-'+id).submit()
         }
+
+
+        $( function() {
+            $( "#sortable" ).sortable({
+                stop: function () {
+                    const dataArr = []
+                    const sorts = document.querySelectorAll('.ui-state-default')
+                    for (let i = 0; i < sorts.length; i++) {
+                        const element = sorts[i]
+                        const parent_sign = element.getAttribute('id')
+                        const sort_id = document.querySelector(`#${parent_sign} .order_sort_id`)
+                        // const sort_id = document.querySelector(`#${parent_sign}`).getAttribute('data-sort-order-id')
+                        const id = document.querySelector(`#${parent_sign} .id`).getAttribute('value')
+                        const text = 1 + i
+                        sort_id.innerText = text
+                        const data = {sort_id: sort_id.innerText, id: id}
+                        dataArr.push(data)
+                    }
+                    $.ajax({
+                        url: "{{route('sortPermission')}}",
+                        type: "POST",
+                        data: {data: dataArr},
+                        dataType: 'json',
+                        success: function (data) {
+                            console.log(data)
+                            if(data.status == 200){
+                                Toast.fire({
+                                    icon: 'success',
+                                    title: data.message
+                                });
+                            }else{
+                                console.log(data)
+                            }
+                        }
+                    })
+                }
+            });
+        } );
+
+
+
     </script>
 @endsection
